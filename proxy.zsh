5b6da69850069bda6c59dd33b9735a4c63a59e12
Akagi201 <akagi201@gmail.com>
2022-02-21 11:15:17 +0800

    add discord

--- proxy.zsh
+++ proxy.zsh
@@ -3,3 +3,4 @@ alias proxylb='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.
 # monocloud proxy
 alias proxymono='export https_proxy=http://127.0.0.1:8890 http_proxy=http://127.0.0.1:8890 all_proxy=socks5://127.0.0.1:8890'
 alias proxyoff='unset https_proxy http_proxy all_proxy'
+alias discord='/Applications/Discord.app/Contents/MacOS/Discord --proxy-server=socks5://127.0.0.1:7890'

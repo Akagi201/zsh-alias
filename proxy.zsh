d144a39edb1df3fda9fb1eef2f1a7b395a0b0b3f
Akagi201 <akagi201@gmail.com>
2022-04-08 22:08:22 +0800

    feat(proxy): remove longbridge proxy

--- proxy.zsh
+++ proxy.zsh
@@ -1,6 +1,4 @@
-# longbridge proxy
-alias proxylb='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 # monocloud proxy
-alias proxymono='export https_proxy=http://127.0.0.1:8890 http_proxy=http://127.0.0.1:8890 all_proxy=socks5://127.0.0.1:8890'
+alias proxymono='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
 alias proxyoff='unset https_proxy http_proxy all_proxy'
 alias discord='/Applications/Discord.app/Contents/MacOS/Discord --proxy-server=socks5://127.0.0.1:7890'

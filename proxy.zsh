6b7ba8dd6f7a2144eb6408c3ae0d03e500284d8c
Bob Liu <akagi201@gmail.com>
2026-01-07 23:48:23 +0800

    feat(proxy): add no_proxy to proxyon alias for local addresses

--- proxy.zsh
+++ proxy.zsh
@@ -1,4 +1,4 @@
 # clash proxy
-alias proxyon='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890'
+alias proxyon='export https_proxy=http://127.0.0.1:7890 http_proxy=http://127.0.0.1:7890 all_proxy=socks5://127.0.0.1:7890 no_proxy="localhost,127.0.0.1,::1,0.0.0.0,.local"'
 alias proxyoff='unset https_proxy http_proxy all_proxy'
 alias discord='/Applications/Discord.app/Contents/MacOS/Discord --proxy-server=socks5://127.0.0.1:7890'
